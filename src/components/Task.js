d6118d33eb6fd307390789809ebbef3fc0f299c8
njoro <mwithiganjoroge@gmail.com>
2021-12-29 16:19:45 +0300

    Completed Tasklist component, stories, and tests

---
+++ src/components/Task.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          name="checked"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+        />
+        <span
+          className="check-box-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div className="title">
+          <input type="text" value={title} readOnly={true} placeholder="Input title"/>
+      </div>
+      <div className="actions" onClick={event => event.stopPropagation()}>
+          {state !== 'TASK_ARCHIVED' && (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a onClick={()=>onPinTask(id)}>
+                <span className="icon-star" id={`pinTask-${id}`} aria-label={`pinTask-${id}`}/>
+            </a>
+          )}
+      </div>
+
+    </div>
+  );
+}
+
+Task.propTypes = {
+    /** composition of teh task*/
+    task: PropTypes.shape({
+        /**ID of the task */
+        id: PropTypes.string.isRequired,
+        /** Title of the task */
+        title: PropTypes.string.isRequired,
+        /** state */
+        state: PropTypes.string.isRequired,
+    }),
+    /**Event to transform the task to archived */
+    onArchiveTask: PropTypes.func,
+    /** Event to change the state to pinned */
+    onPinTask: PropTypes.func,
+}
