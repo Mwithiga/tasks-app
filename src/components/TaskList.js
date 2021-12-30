ea4a70f3d8ea1b01b55b19b5f96863b47375550b
njoro <mwithiganjoroge@gmail.com>
2021-12-30 22:28:17 +0300

    complete taslbox project

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { updateTaskState } from "../lib/store";
+import Task from "./Task";
 
-import Task from './Task';
-
-
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -42,28 +42,50 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     );
   }
   const tasksInOrder = [
-    ...tasks.filter(t => t.state === 'TASK_PINNED'),
-    ...tasks.filter(t => t.state !== 'TASK_PINNED'),
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
   ];
   return (
     <div className="list-items">
-      {tasksInOrder.map(task => (
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
   );
 }
+PureTaskList.propTypes = {
+  /** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of tasks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+};
+PureTaskList.defaultProps = {
+  loading: false,
+};
 
-TaskList.propTypes = {
-    /** Checks if it's in loading state */
-     loading: PropTypes.bool,
-     /** The list of tasks */
-     tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-     /** Event to change the task to pinned */
-     onPinTask: PropTypes.func,
-     /** Event to change the task to archived */
-     onArchiveTask: PropTypes.func,
-    };
-TaskList.defaultProps = {
-     loading: false,
-    };
+export function TaskList() {
+  const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+
+  const pinTask = (value) => {
+    dispatch(updateTaskState({ id: value, newTaskState: "TASK_PINNED" }));
+  };
+  const archiveTask = (value) => {
+    dispatch(updateTaskState({ id: value, newTaskState: "TASK_ARCHIVED" }));
+  };
+
+  const filteredTasks = tasks.filter(
+    (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+  );
+  return (
+    <PureTaskList
+      tasks={filteredTasks}
+      onPinTask={(task) => pinTask(task)}
+      onArchiveTask={(task) => archiveTask(task)}
+    />
+  );
+}
--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,31 +1,29 @@
 import React from 'react';
 
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 import * as TaskStories from './Task.stories';
 
 export default {
-  component: TaskList,
-  title: 'TaskList',
+component: PureTaskList,
+  title: 'PureTaskList',
   decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
 };
 
-const Template = args => <TaskList {...args} />;
+const Template = args => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   // Shaping the stories through args composition.
-  // The data was inherited from the Default story in task.stories.js.
+  // The data was inherited the Default story in task.stories.js.
   tasks: [
-    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1 👇' },
-    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2 ' },
+    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
     { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
     { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
     { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
     { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
   ],
 };
-
-
 
 export const WithPinnedTasks = Template.bind({});
 WithPinnedTasks.args = {
